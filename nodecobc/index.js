59ce840c0e024f8e5710b21260ecb3b57458d409
devvildaz <diegovilca_dev@hotmail.com>
2022-06-05 23:32:18 -0500

    feat: https project

--- nodecobc/index.js
+++ nodecobc/index.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require("cors");
+const https = require("https");
 const db = require('./models/index');
 const {paths : staticPaths} = require("./utils/constants");
 const env = process.env.NODE_ENV || 'development';
 const configDb = require(__dirname + '/config/config.json')[env];
+const fs = require("fs");
+
 
 const util = require('util');
 const router = require('./controllers');
 const exec = util.promisify(require('child_process').exec);
 
 const app = express();
+
 app.use(cors());
 const PORT = 8080;
 
@@ -19,6 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/', router);
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert : fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
+
+
 
 (async () => {
     await db.sequelize.authenticate();
@@ -31,7 +43,7 @@ app.use('/api/v1/', router);
     output = await exec(`cobc -x -lpq -locesql ${staticPaths.ocesqlScripts}/BatchFile.cob -o ${staticPaths.ocesqlExec}/BatchFile`);
     console.log(output.stdout);
 })().then(() => {
-    app.listen(PORT, () => {
+    sslServer.listen(PORT, () => {
         console.log("Deployed on port number",PORT)
         console.log("rootproject:",staticPaths.rootProject)
         console.log("Environment:",env);
